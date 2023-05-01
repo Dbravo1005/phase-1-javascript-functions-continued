f9f73d2379eed95384cd7f980c197765043f5320
Damian Bravo <Dbravo1005@gmail.com>
2023-04-30 23:33:41 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(fun = 'roller-skate'){
+    return (`This Saturday, I want to ${fun}!`)
+};
+
+console.log(saturdayFun(fun = "bathe my dog"));
+
+function mondayWork(work = "go to the office"){
+    return (`This Monday, I will ${work}.`)
+}
+console.log(mondayWork(fun = 'work from the office'))
+
+//outer
+function wrapAdjective(flair = "*"){
+   return function(result = "a hard worker"){
+    return `You are ${flair}${result}${flair}!`
+   };
+}
+
+
+const encouragingPromptFunction = wrapAdjective("*");
+console.log(encouragingPromptFunction());
+console.log(wrapAdjective("%")("a dedicated programmer"));
